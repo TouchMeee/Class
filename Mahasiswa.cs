56476c5cb9cca9eecbf00d0a3186d86c9f0ce744
Zulfikar <muhammad.zulfikar00@gmail.com>
2019-03-19 12:12:50 +0700

    siscon

---
+++ Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Mahasiswa
+    {
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("NIM: {0}", NIM);
+            Console.WriteLine("IPK: {0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+
+        }
+        public void isiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs\n", Nama);
+        }
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.NIM = "11111";
+            saya.Nama = "Jono";
+            saya.Ipk = 6.77f;
+
+            kamu.NIM = "33333";
+            kamu.Nama = "Paijo";
+            kamu.Ipk = 8.90f;
+
+            saya.Registrasi();
+            saya.isiKrs();
+
+            kamu.Registrasi();
+            kamu.isiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
